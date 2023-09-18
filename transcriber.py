6ea0dfd05d8bbb6d05c5f66c987949d0a9bb702c
Renseck <rensvaneck@gmail.com>
2023-09-18 14:53:46 +0200

    Added comments

--- transcriber.py
+++ transcriber.py
@@ -21,7 +21,6 @@ from deepmultilingualpunctuation import PunctuationModel
 verbose = True
 SetLogLevel(-1)
 
-splitPath = "SplitAudio/"
 monoPath = "MonoAudio/"
 
 def hr_min_sec(secs):
@@ -35,7 +34,7 @@ def hr_min_sec(secs):
 
     Returns
     -------
-    str
+    STR
         {hours:02d}:{minutes:02d}:{secs:02d}
 
     """
@@ -44,16 +43,32 @@ def hr_min_sec(secs):
     return f"{hours:02d}:{minutes:02d}:{secs:02d}"
 
 def convert_to_wav(input_file):
+    """
+    Converts whatever (?) filetype to .wav for further use.
+
+    Parameters
+    ----------
+    input_file : STR
+        Name of the file, with extension.
+
+    Returns
+    -------
+    STR
+        New name of the file, i.e. with the .wav extension. 
+
+    """
     input_split = input_file.split(".")
     input_ext = input_split[-1]
     wav_filename = input_split[0] + ".wav"
     
+    #I've yet to run into a filetype which cannot be converted this way
     sound = AudioSegment.from_file(input_file, format = input_ext)
     file_handle = sound.export(wav_filename, format = "wav")
     
     return file_handle.name
 
 def split_wav(wav_file):
+    # Currently unused - delete?
     sound = AudioSegment.from_file(wav_file, format = "wav")
     chunks = split_on_silence(sound, min_silence_len = 100, silence_thresh = -80, keep_silence = 200)
 
@@ -73,6 +88,22 @@ def split_wav(wav_file):
     print(f"[INFO] Split into {i+1} sub-clips.")
 
 def wav_to_mono(wav_file, mono_wav_file):
+    """
+    Converts .wav files to mono channel format.
+
+    Parameters
+    ----------
+    wav_file : STR
+        File name of a .wav file, including extension.
+    mono_wav_file : STR
+        File name of the mono .wav file, including extension.
+
+    Returns
+    -------
+    INT
+        Number of sound bytes in the file (used for loading bars)
+
+    """
     try:
         inFile = wave.open(wav_file, "rb")
         outFile = wave.open("MonoAudio/"+mono_wav_file, "wb")
@@ -97,6 +128,20 @@ def wav_to_mono(wav_file, mono_wav_file):
     return len(soundBytes)
 
 def capitalize_sentences(text):
+    """
+    Capitalises the first letter of every sentence in a string.
+
+    Parameters
+    ----------
+    text : STR
+        Not nicely capitalised string.
+
+    Returns
+    -------
+    result_text : STR
+        Nicely capitalised string.
+
+    """
     # Split the text into sentences using a period (.) as the delimiter
     sentences = re.split(r'\.', text)
     
@@ -117,7 +162,23 @@ def capitalize_sentences(text):
     result_text = '.'.join(sentences)
     return result_text
 
-def insert_newlines(text, max_line_length):
+def insert_newlines(text, max_line_length = 80):
+    """
+    Clips lines to a maximum character length and inserts a new line \n
+
+    Parameters
+    ----------
+    text : STR
+        Any run-on string.
+    max_line_length : INT
+        Maximum length of text line, default is 80
+
+    Returns
+    -------
+    STR
+        String with enters added in.
+
+    """
     lines = []
     words = text.split()
     current_line = ""
@@ -135,6 +196,14 @@ def insert_newlines(text, max_line_length):
     return "\n".join(lines)
 
 def make_folders():
+    """
+    Generates the subfolder in which the mono audio is temporarily kept.
+
+    Returns
+    -------
+    None.
+
+    """
     flag = False
     monoPathExist = os.path.exists(monoPath)
     
@@ -148,6 +217,14 @@ def make_folders():
         print("Cleared errant files from temp directories.")
 
 def delete_stragglers():    
+    """
+    Gets rid of temps files to save space.
+
+    Returns
+    -------
+    None.
+
+    """
     for monoFile in os.listdir(monoPath):
         os.remove(os.path.join(monoPath, monoFile))
     
@@ -182,8 +259,11 @@ def main():
     wf = wave.open(os.path.join(monoPath, monoFileName))
     
     ##### Run model to recognise language #####
-    model = Model(model_path = "vosk-model-nl-spraakherkenning-0.6")
-    # model = Model(lang = "nl")
+    if "vosk-model-nl-spraakherkenning-0.6"  in os.listdir(os.getcwd()):
+        model = Model(model_path = "vosk-model-nl-spraakherkenning-0.6")
+    else:
+        print("Large Dutch model not found - using the default Dutch model. See README for more info")
+        model = Model(lang = "nl")
     
     recognizer = KaldiRecognizer(model, wf.getframerate())
     recognizer.SetWords(True)
